46aaa617f69f7c087cfea8abef042e684695e824
Aleksa <aleksastanitch@gmail.com>
2019-08-23 16:12:37 +0200

    adapted >576px  reviews section

--- src/js/script.js
+++ src/js/script.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 	$('.carousel__inner').slick({
 		speed: 1000,
 		adaptiveHeight: false,
-		autoplay: true,
+		autoplay: false,
 		autoplaySpeed: 2000,
 		prevArrow: '<button type="button" class="slick-prev"><img src="../icons/left.svg"></button>',
 		nextArrow: '<button type="button" class="slick-next"><img src="../icons/right.svg"></button>',
