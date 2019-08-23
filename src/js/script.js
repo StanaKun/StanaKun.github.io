4a69e338149bfbff58c4309c418e966d3e95c098
Aleksa <aleksastanitch@gmail.com>
2019-08-23 18:12:55 +0200

    add footer

--- src/js/script.js
+++ src/js/script.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 	$('.carousel__inner').slick({
 		speed: 1000,
 		adaptiveHeight: false,
-		autoplay: false,
+		autoplay: true,
 		autoplaySpeed: 2000,
 		prevArrow: '<button type="button" class="slick-prev"><img src="../icons/left.svg"></button>',
 		nextArrow: '<button type="button" class="slick-next"><img src="../icons/right.svg"></button>',
